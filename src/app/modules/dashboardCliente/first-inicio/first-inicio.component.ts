87c4895f3fde9d663be02918d75db2f780408717
Jedidiaz <jedidiazfagundez@gmail.com>
2023-03-14 13:56:56 -0500

    cambios filtros

--- src/app/modules/dashboardCliente/first-inicio/first-inicio.component.ts
+++ src/app/modules/dashboardCliente/first-inicio/first-inicio.component.ts
@@ -13,8 +13,17 @@ export class FirstInicioComponent implements OnInit {
   dos: boolean = false;
   tres: boolean = false;
   cuatro: boolean = false;
+  cinco: boolean = false;
+  seis: boolean = false;
+  siete: boolean = false;
+  ocho: boolean = false;
+  nueve: boolean = false;
+  dies: boolean = false;
+  once: boolean = false;
+  doce: boolean = false;
+
   nombre: any = 'Nombre';
-  preferencias: any = {especialidad: '', genero: '', edad: '', precio: ''}
+  preferencias: any = {terapia: [], genero: '', sesion: '', horario: []}
 
   constructor(
     private formbuilder: FormBuilder,
@@ -25,6 +34,8 @@ export class FirstInicioComponent implements OnInit {
       genero: ['', Validators.required],
       edad: ['', Validators.required],
       precio: ['', Validators.required],
+      hora: ['', Validators.required],
+      sesion: ['', Validators.required],
     });
   }
   ngOnInit(): void {}
@@ -45,7 +56,14 @@ export class FirstInicioComponent implements OnInit {
     this.dos = true;
     this.tres = false;
     this.cuatro = false;
-    this.preferencias.especialidad = target
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
   }
 
   next1(target: any) {
@@ -53,7 +71,15 @@ export class FirstInicioComponent implements OnInit {
     this.dos = false;
     this.tres = true;
     this.cuatro = false;
-    this.preferencias.genero = target
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+    this.preferencias.sesion = target
   }
 
   next2(target: any) {
@@ -61,16 +87,148 @@ export class FirstInicioComponent implements OnInit {
     this.dos = false;
     this.tres = false;
     this.cuatro = true;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+  }
+
+  next3(target: any) {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = true;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
     this.preferencias.genero = target
   }
 
-  prefer(target: any) {
-    console.log(target)
+  next4(target: any) {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = true;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+    this.preferencias.horario = target
+  }
+
+  next5() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = true;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+    // this.preferencias.genero = target
+  }
+
+  next6() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = true;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+    // this.preferencias.genero = target
+  }
+
+  next7() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = true;
+    this.dies = false;
+    this.once = false;
+    this.doce = false;
+    // this.preferencias.genero = target
+  }
+
+  next8() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = true;
+    this.once = false;
+    this.doce = false;
+    // this.preferencias.genero = target
+  }
+
+  next9() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = true;
+    this.doce = false;
+    // this.preferencias.genero = target
+  }
+
+  next10() {
+    this.uno = false;
+    this.dos = false;
+    this.tres = false;
+    this.cuatro = false;
+    this.cinco = false;
+    this.seis = false;
+    this.siete = false;
+    this.ocho = false;
+    this.nueve = false;
+    this.dies = false;
+    this.once = false;
+    this.doce = true;
+    // this.preferencias.genero = target
+  }
+
+  prefer() {
     const data = `${this.formPrefer.value.edad},${this.formPrefer.value.genero},${this.formPrefer.value.precio},${this.formPrefer.value.especialidad}`;
     console.log(data)
-    const form = new FormData();
-    form.append('preferencia', data);
-    this.apiService.preferencia(form).subscribe({
+    this.apiService.preferencia(this.preferencias).subscribe({
       next: (res)=> {
         console.log(res)
         if (res.response === 'Success') window.location.reload()
@@ -78,5 +236,10 @@ export class FirstInicioComponent implements OnInit {
         console.log(err)
       }
     })
+  }
+
+  especial(target: any){
+    this.preferencias.terapia.push(target.value)
+    console.log(this.preferencias.terapia)
   }
 }
